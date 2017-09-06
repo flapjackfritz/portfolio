7bec235ed9604e1d44caf8d59a2895fb1aaac3df
Tommy Fritz <thomasgfritz@gmail.com>
2017-09-06 17:15:09 -0500

    Add webpack and setup react and babel config

--- deploy_portfolio_lambda.py
+++ deploy_portfolio_lambda.py
@@ -3,46 +3,47 @@ from botocore.client import Config
 import StringIO
 import zipfile
 
+
 def lambda_handler(event, context):
-  sns = boto3.resource('sns')
-  topic = sns.Topic('arn:aws:sns:us-east-2:784236863444:deploy_portfolio_topic')
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-2:784236863444:deploy_portfolio_topic')
 
-  location = {
-    "bucketName": 'staging.tommyfritz.com',
-    "objectKey": 'portfoliobuild.zip'
-  }
+    location = {
+        "bucketName": 'staging.tommyfritz.com',
+        "objectKey": 'portfoliobuild.zip'
+    }
 
-  try:
-    job = event.get("CodePipeline.job")
+    try:
+        job = event.get("CodePipeline.job")
 
-    if job:
-      for artifact in job["data"]["inputArtifacts"]:
-        if artifact["name"] == "MyAppBuild":
-          location = artifact["location"]["s3Location"]
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "MyAppBuild":
+                    location = artifact["location"]["s3Location"]
 
-    print "Building portfolio from " + str(location)
+        print "Building portfolio from " + str(location)
 
-    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-    portfolio_bucket = s3.Bucket('portfolio.tommyfritz.com')
-    build_bucket = s3.Bucket(location["bucketName"])
+        portfolio_bucket = s3.Bucket('portfolio.tommyfritz.com')
+        build_bucket = s3.Bucket(location["bucketName"])
 
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
 
-    with zipfile.ZipFile(portfolio_zip) as zipped_portfolio:
-      for name in zipped_portfolio.namelist():
-        obj = zipped_portfolio.open(name)
-        portfolio_bucket.upload_fileobj(obj, name)
-        portfolio_bucket.Object(name).Acl().put(ACL='public-read')
+        with zipfile.ZipFile(portfolio_zip) as zipped_portfolio:
+            for name in zipped_portfolio.namelist():
+                obj = zipped_portfolio.open(name)
+                portfolio_bucket.upload_fileobj(obj, name)
+                portfolio_bucket.Object(name).Acl().put(ACL='public-read')
 
-    print "Job Done!"
-    topic.publish(Subject="Portfolio Deployed", Message="The portfolio deployed successfully.")
+        print "Job Done!"
+        topic.publish(Subject="Portfolio Deployed", Message="The portfolio deployed successfully.")
 
-    if job:
-      codepipeline = boto3.client('codepipeline')
-      codepipeline.put_job_success_result(jobId=job["id"])
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
 
-  except:
-    topic.publish(Subject="Portfolio Deploy Failed", Message="The portfolio was not deployed successfully.")
-    raise
+    except:
+        topic.publish(Subject="Portfolio Deploy Failed", Message="The portfolio was not deployed successfully.")
+        raise
